065fff7b09de3c1f4bd7ffdc1e12553adccb4832
redwanweb353 <redwanweb353@gmail.com>
2025-08-30 21:42:49 +0600

    added heart click js function

--- emergency.js
+++ emergency.js
@@ -1,1 +1,11 @@
 
+const heartCount = document.getElementById('heart-icon-number')
+let iconButtons = document.getElementsByClassName('heart-icon-btn')
+let currentNumber= 0;
+    for(const iconButton of iconButtons){
+        iconButton.addEventListener('click', function(){
+         currentNumber++;  
+         heartCount.innerText = currentNumber
+         
+    })
+ }
