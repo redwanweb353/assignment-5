cc49ff682de2c9d53896213bb4c19a65efa0167d
redwanweb353 <redwanweb353@gmail.com>
2025-08-30 21:45:50 +0600

    call btn alart section js file added

--- emergency.js
+++ emergency.js
@@ -9,3 +9,23 @@ let currentNumber= 0;
          
     })
  }
+
+ 
+const helplineCallButtons = document.getElementsByClassName('helpline-call-btn')
+ 
+
+for ( let helplineCallButton of helplineCallButtons){
+        helplineCallButton.addEventListener('click', function () {
+        let totalCoin = parseInt(document.getElementById('coin-balance').innerText)
+         if(totalCoin < 20){
+                alert('Insuficiant balance')
+                return;
+            }
+        totalAvailableCoin = totalCoin - 20;
+        document.getElementById('coin-balance').innerText = totalAvailableCoin;
+           
+        const serviceTitle = helplineCallButton.parentNode.parentNode.childNodes[3].childNodes[3].innerText
+        const serviceNumber = helplineCallButton.parentNode.parentNode.childNodes[3].childNodes[5].innerText
+        alert(`📞 Calling ${ serviceTitle} ${serviceNumber} .... `)
+        })
+}
